e7d89ed1cf6317b71bd83a114c425b7c28d3ed12
Rottschaefer <rottschaefer54@gmail.com>
2023-06-28 20:13:10 -0300

    Seção 1 implementada parcialmente

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,28 @@
-import { StyledHeader } from "./StyledHeader";
+import {
+  StyledButton,
+  StyledHeader,
+  StyledLogo,
+  StyledLogoBox,
+  StyledSubTitle,
+  StyledText,
+  StyledTextBox,
+  StyledTitle,
+} from "./StyledHeader";
+import logo from "../../img/logo-white.png";
 
 export const Header = () => {
-  return <StyledHeader>Header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <StyledLogoBox>
+        <StyledLogo src={logo} alt="Logo" />
+      </StyledLogoBox>
+      <StyledTextBox>
+        <StyledText>
+          <StyledTitle>Outdoors</StyledTitle>
+          <StyledSubTitle>is where life happens</StyledSubTitle>
+        </StyledText>
+        <StyledButton href="#">Discover our tours</StyledButton>
+      </StyledTextBox>
+    </StyledHeader>
+  );
 };
--- src/Components/Header/StyledHeader.js
+++ src/Components/Header/StyledHeader.js
@@ -1,8 +1,103 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import hero from "../../img/hero.jpg";
 
+// Light green: #7ed56f
+// Medium green: #55c57a
+// Dark green: #28b485
+
+const moveInDown = keyframes`
+0%{
+  opacity: 0;
+  transform: translateY(-30px);
+}
+100%{
+  opacity: 1;
+  transform: translateY(0);
+}
+`;
+
+const moveInUp = keyframes`
+0%{
+  opacity: 0;
+  transform: translateY(30px);
+}
+100%{
+  opacity: 1;
+  transform: translateY(0);
+}
+`;
+
 export const StyledHeader = styled.header`
-  background-image: url(${hero});
+  position: relative;
+  background-image: linear-gradient(
+      to right,
+      rgb(126, 213, 111, 0.8),
+      rgb(40, 180, 133, 0.8)
+    ),
+    url(${hero});
   background-size: cover;
   height: 95vh;
+  background-position: top;
+  clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
+`;
+
+export const StyledLogoBox = styled.div`
+  position: absolute;
+  top: 40px;
+  left: 40px;
+`;
+
+export const StyledLogo = styled.img`
+  height: 35px;
+`;
+
+export const StyledTextBox = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+
+  backface-visibility: hidden;
+`;
+
+export const StyledText = styled.h1`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+export const StyledTitle = styled.span`
+  width: 98%;
+  letter-spacing: 35px;
+  font-size: 60px;
+  font-weight: 400;
+  animation: ${moveInDown} 1.5s ease-out;
+`;
+
+export const StyledSubTitle = styled.span`
+  width: 98%;
+  font-size: 20px;
+  font-weight: 700;
+  letter-spacing: 17.4px;
+  padding-right: -17.4px;
+
+  animation: ${moveInUp} 1.5s ease-out;
+`;
+
+export const StyledButton = styled.a`
+  color: #777;
+  /* font-size: 18px; */
+  text-decoration: none;
+  background-color: #fff;
+  padding: 19px 40px;
+  display: inline-block;
+  margin-top: 50px;
+  border-radius: 100px;
+  font-weight: 400;
+  text-transform: uppercase;
 `;
